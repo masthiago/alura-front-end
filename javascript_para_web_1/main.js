c206dacf42cd9b5d7fed12901b0ba45ca9a025f2
Thiago <thiago@thiago-98e9b34d.tsr.net.br>
2023-01-02 20:28:28 -0300

    javascript_para_web_1 - 2

--- javascript_para_web_1/main.js
+++ javascript_para_web_1/main.js
@@ -1,1 +1,11 @@
-var tecla_pom = document.querySelector('.tecla_pom');
+function tocaSom (som) {
+    som_id =  '#som_tecla_' + som;
+    document.querySelector(som_id).play();
+}
+
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
